3e168f69a6a46947106ddfa376e461b81663bb26
LukaErcegovac <lukaileon@gmail.com>
2022-03-21 17:07:37 +0100

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,10 @@
   <div class="app">
     <nav id ="nav" class="navbar navbar-expand-lg navbar-light bg-light">
       <a class="navbar-brand" href="#">
-        <img src="@/assets/Fipugram.png" alt=""  height="40" class="d-inline-block align-text-top">
+        <img src="@/assets/Fipugram.png" height="40" class="d-inline-block align-top" alt="" loading="lazy" />
       </a>
 
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggler" aria-controls="navbarToggler" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
 
